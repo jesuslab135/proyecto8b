3b11a517ecb208e848cc61e7200aa7b875aed347
Juanita456 <ajdomind@gmail.com>
2025-07-23 12:53:09 -0700

    actualizacion

--- api/src/middlewares/authMiddleware.ts
+++ api/src/middlewares/authMiddleware.ts
@@ -3,6 +3,7 @@ import jwt from 'jsonwebtoken';
 
 export function verifyToken(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.header('Authorization');
+<<<<<<< HEAD
 
    if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ error: 'Access denied' });
@@ -10,8 +11,14 @@ export function verifyToken(req: Request, res: Response, next: NextFunction) {
 
   const token = authHeader.split(' ')[1]; // Extrae solo el token puro
 
+=======
+  if (!authHeader) {
+    res.status(401).json({ error: 'Access denied' });
+    return;
+  }
+  const token = authHeader.startsWith('Bearer ') ? authHeader.slice(7) : authHeader;
+>>>>>>> b6959c3 (actualizacion)
   try {
-    // decode jwt toke data
     const decoded = jwt.verify(token, 'your-secret');
     if (typeof decoded !== 'object' || !decoded?.userId) {
       res.status(401).json({ error: 'Access denied' });
@@ -26,12 +33,18 @@ export function verifyToken(req: Request, res: Response, next: NextFunction) {
 }
 
 export function verifyAdmin(req: Request, res: Response, next: NextFunction) {
+<<<<<<< HEAD
   const role = req.rol_id;
   if (role !== 1) {
+=======
+  const role = req.role;
+  if (role !== 'admin') {
+>>>>>>> b6959c3 (actualizacion)
     res.status(401).json({ error: 'Access denied' });
     return;
   }
   next();
+<<<<<<< HEAD
 }
 
 export function verifyUni(req: Request, res: Response, next: NextFunction) {
@@ -41,4 +54,6 @@ export function verifyUni(req: Request, res: Response, next: NextFunction) {
     return;
   }
   next();
+=======
+>>>>>>> b6959c3 (actualizacion)
 }
--- api/src/routes/auth/index.ts
+++ api/src/routes/auth/index.ts
@@ -13,7 +13,11 @@ import jwt from 'jsonwebtoken';
 const router = Router();
 
 const generateUserToken = (user: any) => {
+<<<<<<< HEAD
   return jwt.sign({ userId: user.id, rol_id: user.rol_id }, 'your-secret', {
+=======
+  return jwt.sign({ userId: user.id, role: user.role }, 'your-secret', {
+>>>>>>> b6959c3 (actualizacion)
     expiresIn: '30d',
   });
 };
@@ -76,16 +80,29 @@ const generateUserToken = (user: any) => {
  */
 router.post('/register', validateData(insertUsuarioSchema), async (req, res) => {
   try {
+<<<<<<< HEAD
     const {id, ...data} = req.cleanBody;
     data.contrasena = await bcrypt.hash(data.contrasena, 10);
+=======
+    const data = req.cleanBody;
+    console.log('Datos recibidos:', data);
+    data.password = await bcrypt.hash(data.password, 10);
+>>>>>>> b6959c3 (actualizacion)
 
     const [user] = await db.insert(usuariosTable).values(data).returning();
 
     // @ts-ignore
+<<<<<<< HEAD
     delete user.contrasena;
     const token = generateUserToken(user);
 
     res.status(201).json({ token, user });
+=======
+    delete user.password;
+    const token = generateUserToken(user);
+
+    res.status(201).json({ user, token });
+>>>>>>> b6959c3 (actualizacion)
   } catch (e) {
     console.log(e);
     res.status(500).send('Something went wrong');
@@ -146,9 +163,16 @@ router.post('/login', validateData(loginSchema), async (req, res) => {
       return;
     }
 
+<<<<<<< HEAD
     const token = generateUserToken(user);
     // @ts-ignore
     delete user.contrasena;
+=======
+    // create a jwt token
+    const token = generateUserToken(user);
+    // @ts-ignore
+    delete user.password;
+>>>>>>> b6959c3 (actualizacion)
     res.status(200).json({ token, user });
   } catch (e) {
     res.status(500).send('Something went wrong');
